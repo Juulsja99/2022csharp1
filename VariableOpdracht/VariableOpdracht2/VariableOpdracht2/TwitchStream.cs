135589519286d93d8fddea27f5641af41ddd4b94
Julie Jaasma <31329@ma-web.nl>
2023-03-06 09:13:14 +0100

    update

---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+
+        string name = "Shannon Woodward";
+        string LastStreamName = "God of war 2018 PC!";
+        Game streamGame = (new Game("God of war?"));
+        int followers = 31900;
+        bool autoMod = false;
+
+         
+        //maak hier class variables aan:
+        // type=string, naam: name,
+        // type=string, naam: lastStreamName
+        // type=Game, naam: streamGame
+        // type=int, naam: followers
+        // type=bool, naam: automod
+        
+    }
+}
