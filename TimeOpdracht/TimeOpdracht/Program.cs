4aae074446e2879f5daeb10c673d0a183c2b4aa8
Julie Jaasma <31329@ma-web.nl>
2023-04-14 09:30:01 +0200

    Time

---
+++ TimeOpdracht/TimeOpdracht/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TimeOpdracht
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+           while(true) 
+           {
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.WriteLine("Het is nu {0}", time);
+                Thread.Sleep(1000);
+                Console.Clear();
+           }
+        }
+    }
+}
